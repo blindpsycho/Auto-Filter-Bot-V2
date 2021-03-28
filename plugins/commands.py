f950b1d002f2a6f977b7a932f09e9be596198fac
amegh-amz <80452444+amegh-amz@users.noreply.github.com>
2021-03-28 13:48:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://t.me/amzmtaccountt)
+                            "SOURCE CODE", url="https://t.me/amzmtaccountt")
                     ]
                 ]
             ),
